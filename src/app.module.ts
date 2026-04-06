6294ea57631d139b4c26b08944475a706c345e1e
Targib Sajid <167701006+TargibSajid@users.noreply.github.com>
2026-04-07 01:32:13 +0600

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule,TypeOrmModule.forRoot(
+{ type: 'postgres',
+host: 'localhost',
+port: 5432,
+username: 'postgres',
+password: '',
+database: 'equipment_rental',//Change to your database name
+autoLoadEntities: true,
+synchronize: true,
+} )],
   controllers: [AppController],
   providers: [AppService],
 })
